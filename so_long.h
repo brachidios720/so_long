189dc48b7be7b1a351778ef4c2b6fd81f88a5890
brachidios720 <raphael.carbonnnel@gmail.com>
2023-12-13 12:02:56 +0100

    exit

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: rcarbonn <rcarbonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 19:22:49 by rcarbonn          #+#    #+#             */
-/*   Updated: 2023/12/13 01:28:59 by rcarbonn         ###   ########.fr       */
+/*   Updated: 2023/12/13 11:58:36 by rcarbonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,32 @@ typedef struct s_game
 {
 	char **map;
 	char *filename;
-	
+	int	 player;
+	int		x;
+	int		y;
+	int		width;
+	int		height;
+	void	sprit_player;
+	void	sprit_end;
+	void	sprit_wall;
+	void	sprit_collectible;
+	void	sprit_enemy;
+	void	sprit_rock;
+	int		image_size;
 }				t_game;
+
+// inttiate struct
+
+// open file 
+
+// maps checks
+
+// Prasing
+
+// render & key
+
+//exit
+void	ft_print_error(int err);
+void	ft_free_map(t_game *game);
 
 #endif
