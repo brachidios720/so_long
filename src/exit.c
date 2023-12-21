04e53487d038615d13464c877e9aa425acf396fa
brachidios720 <raphael.carbonnnel@gmail.com>
2023-12-21 16:48:35 +0100

    sdssd

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: rcarbonn <rcarbonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 23:58:22 by rcarbonn          #+#    #+#             */
-/*   Updated: 2023/12/19 12:04:15 by rcarbonn         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:34:39 by rcarbonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_free_map(t_game *game)
 	i = -1;
 	if (game->map)
 	{
-		while(++i < game->map_hight)
+		while (++i < game->map_hight)
 			free(game->map[i]);
 		free(game->map);
 	}
@@ -28,8 +28,8 @@ void	ft_free_map(t_game *game)
 void	ft_print_error(int err)
 {
 	if (err == ERROR_MALLOC)
-		ft_putstr_fd("Error\nMalloc_Failed\n", 2)
-	if (err == )
+		ft_putstr_fd("Error\nMalloc_Failed\n", 2) 
+	if (err ==)
 }
 
 static void	ft_clean_sprite(t_game *game)
--- src/main.c
+++ src/main.c
@@ -6,16 +6,43 @@
 /*   By: rcarbonn <rcarbonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 19:22:39 by rcarbonn          #+#    #+#             */
-/*   Updated: 2023/12/13 01:18:40 by rcarbonn         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:45:07 by rcarbonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	init_game(t_game *game, char **argv)
+{
+	game->filename = argv[1];
+	game->player = 0
+	game->x = 0;
+	game->y = 0;
+	game->end = 0;
+	game->nb_move = 0;
+	game->width = 0;
+	game->height = 0;
+	game->sprite_player_1 = NULL;
+	game->sprite_player_2 = NULL;
+	game->sprite_end = NULL;
+	game->sprite_collectible = NULL;
+	game->sprite_air = NULL;
+	game->sprite_rock = NULL;
+	game->mlx = mlx_init();
+	if(game->mlx != mlx_init)
+		ft_exit()
+	game->window = NULL;
+	game->image_size = 0;
+	game->sprite_sun = NULL;
+	game->sprite_bat_danger = NULL;
+}
+
 int	main(int argc, char **argv)
 {
 	t_game *game;
-
+	void	*mlx;
+	
+	mlx = mlx_init();
 	if (argc != 2)
 		return ();
 	game = (t_game *)malloc(sizeof(t_game));
